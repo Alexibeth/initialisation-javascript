ed93fb4d8191811fe405f6e967364ae23366302b
Alexibeth <aleracrespo@gmail.com>
2024-03-03 15:29:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('connected !');
+
+document.getElementById('titre').style.color = "green"
+
+let score = 0; //I store a number
+console.log(score);
+
+score = score + 1; //operation
+console.log(score, "after addition")
+
+//I store a string
+const cours = "Javascript"
+//I check
+console.log(cours, "course");
+
+//I concatenate
+const word1 ="Hello";
+const word2 = "World!";
+const sentence = word1 + " " + word2  //concatenation
+console.log(sentence, "sentence"); // I check
+
+//Calculation
+let calcul = "0"; //I store
+calcul = calcul + 1; //Addition
+console.log(calcul, "calcul"); //Je vérifie
